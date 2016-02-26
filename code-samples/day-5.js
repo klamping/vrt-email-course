8dd00f3c685a9aacb0599cd22263f1cbe7dab008
Kevin Lamping <klamping@gmail.com>
2016-02-26 14:49:00 -0600

    Updates to assertions. Add missing init/end calls

--- code-samples/day-5.js
+++ code-samples/day-5.js
@@ -13,18 +13,31 @@ require("webdrivercss").init(browser, {
   screenWidth: [320,480,640,1024]
 });
 
-function assertShots (err, shot) {
+function assertShots (err, shots) {
   assert.ifError(err);
-  // much more complex code assertion goes here
-}
+
+  Object.keys(shots).forEach(function(element) {
+    shots[element].forEach(function(shot) {
+      assert.ok(shot.isWithinMisMatchTolerance, shot.message);
+    })
+  });
+};
+
+var loginForm = {
+  name: "Login",
+  selector: "form.login"
+};
+
+var mainNav = ".header .nav";
 
 browser
+  .init()
   .url("http://visualregressiontesting.com")
   .getTitle()
   .then(function(title){
-    assert.equal(title, "Visual Regression Testing");
+    assert.equal(title, "Visual Regression Testing - Home");
   })
-  .isVisible(".main-nav")
+  .isVisible(mainNav)
   .then(function(isNavVisible){
     assert.ok(isNavVisible);
   })
@@ -32,4 +45,5 @@ browser
   .setValue(".login .username", "admin")
   .webdrivercss("Login Username", loginForm,  assertShots)
   .setValue(".login .password", "hunter2")
-  .webdrivercss("Login Username Password", loginForm, assertShots);
+  .webdrivercss("Login Username Password", loginForm, assertShots)
+  .end();
